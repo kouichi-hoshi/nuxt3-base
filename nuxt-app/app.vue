95f05d70f98b11415d0cf8d73c02f1321272408c
kouichi.hoshi <kouichi.hoshi@gmail.com>
2022-06-20 18:51:38 +0900

    ページ遷移時インタラクション 実装

--- nuxt-app/app.vue
+++ nuxt-app/app.vue
@@ -9,3 +9,15 @@
     </main>
   </div>
 </template>
+
+<style lang="scss">
+//ページ遷移時インタラクション
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s;
+}
+.page-enter,
+.page-leave-active {
+  opacity: 0;
+}
+</style>
