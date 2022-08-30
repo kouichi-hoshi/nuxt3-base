807d2b52479335dd27438dbd1de9596980642e0b
kouichi.hoshi <kouichi.hoshi@stella-design.biz>
2022-08-30 16:28:52 +0900

    compHeader追加

---
+++ v2/components/compHeader.vue
@@ -0,0 +1,37 @@
+<script setup>
+const props = defineProps({
+  tagName: {
+    type: String,
+    default: 'h2'
+  },
+  // class: {
+  //   type: String,
+  //   default: 'mb-6 md:mb-12'
+  // },
+  innerClass: {
+    type: String,
+    default: ''
+  },
+  removeDefaultClass: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+
+<template>
+  <header :class="{ 'c-header': !removeDefaultClass }">
+    <component :is="tagName" class="c-header__inner" :class="innerClass">
+      <slot></slot>
+    </component>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/css/mixin.scss';
+@use '../assets/css/variables.scss';
+
+.c-header {
+  @include mixin.c-header();
+}
+</style>
