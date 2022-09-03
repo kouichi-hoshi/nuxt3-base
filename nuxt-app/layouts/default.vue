45f340d9a82c3c7f1e75ba04c86432d0bd877dbc
kouichi.hoshi <kouichi.hoshi@stella-design.biz>
2022-09-03 18:51:03 +0900

    defaultレイアウトにトップに戻るボタンを追加

--- nuxt-app/layouts/default.vue
+++ nuxt-app/layouts/default.vue
@@ -48,6 +48,40 @@ useHead({
       </nav>
     </footer>
 
+    <!-- TODO: SVGアイコンを分離する -->
+    <div ref="topReturnBtn" class="text-right">
+      <compButton
+        v-scroll-to="'body'"
+        :href="'#'"
+        class="top-return-btn c-button--circle fixed right-2 bottom-4 md:right-12 md:bottom-8"
+        label=""
+      >
+        <svg
+          width="2rem"
+          height="2rem"
+          viewBox="0 0 24 24"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+        >
+          <title>up_line</title>
+          <g id="" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+            <g id="Arrow" transform="translate(-432.000000, 0.000000)">
+              <g id="up_line" transform="translate(432.000000, 0.000000)">
+                <rect id="" fill-rule="nonzero" x="0" y="0" width="24" height="24"></rect>
+                <path
+                  id=""
+                  d="M11.2929,8.2928 C11.6834,7.90228 12.3166,7.90228 12.7071,8.2928 L18.364,13.9497 C18.7545,14.3402 18.7545,14.9733 18.364,15.3639 C17.9734,15.7544 17.3403,15.7544 16.9497,15.3639 L12,10.4141 L7.05025,15.3639 C6.65973,15.7544 6.02656,15.7544 5.63604,15.3639 C5.24551,14.9733 5.24551,14.3402 5.63604,13.9497 L11.2929,8.2928 Z"
+                  fill="#09244BFF"
+                  class="up-arrow"
+                ></path>
+              </g>
+            </g>
+          </g>
+        </svg>
+      </compButton>
+    </div>
+
     <modalWindow>
       <ul class="modal-nav text-2xl sm:text-3xl md:text-4xl">
         <compNavigation outer-class="modal-window" :links="linkData" />
